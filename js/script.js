86edb0a726371ffa2ec5ef21009cbeedd1969458
lam small water <21657386@life.hkbu.edu.hk>
2022-08-06 14:40:26 +0800

    4.0v

--- js/script.js
+++ js/script.js
@@ -41,16 +41,32 @@ var swiper = new Swiper(".reviews-slider", {
 });
 /* plugin */
 
-let loadMoreBtn = document.querySelector('.packages .load-more .btn');
-let currentItem = 3;
+// let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+// let currentItem = 3;
 
-    loadMoreBtn.onclick = () =>{
-    let boxes = [...document.querySelectorAll('.packages .box-container .box')];
-    for (var i = currentItem; i < currentItem + 3; i++){
-        boxes[i].style.display = 'inline-block';
-    };
-    currentItem += 3;
-    if(currentItem >= boxes.length){
-        loadMoreBtn.style.display = 'none';
-    }
-}
+//     loadMoreBtn.onclick = () =>{
+//     let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+//     for (var i = currentItem; i < currentItem + 3; i++){
+//         boxes[i].style.display = 'inline-block';
+//     };
+//     currentItem += 3;
+//     if(currentItem >= boxes.length){
+//         loadMoreBtn.style.display = 'none';
+//     }
+// }
+
+/* switch fixed & absolute*/
+// let collect = document.querySelector('.products .img-area .collect');
+// let content = document.querySelector('.products .details .content');
+// let collectTop = collect.offsetTop;
+// console.log(collectTop);
+// let contentTop = content.offsetTop;
+// console.log(contentTop);
+
+// document.addEventListener('scroll', ()=>{
+//     if (window.pageYOffset >= collectTop) {
+//         content.style.position = 'absolute';
+//     }
+// })
+
+/* switch fixed & absolute*/
