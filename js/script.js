77b42c9f53d16dd4248e96f2c3b1ac4ddd9536bd
lam small water <21657386@life.hkbu.edu.hk>
2022-08-06 23:34:16 +0800

    5.0v

--- js/script.js
+++ js/script.js
@@ -10,8 +10,31 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+let filterBtn = document.querySelector('#f-btn');
+let closeBtn = document.querySelector('#filter-btn');
+let filterBar = document.querySelector('#whole');
+let mark = document.querySelector('.mark-bg')
+
+filterBtn.onclick = () => {
+    filterBar.style.display = 'flex';
+
+}
+
+mark.onclick = () => {
+    filterBar.style.display = 'none';
+
+}
 
 
+closeBtn.onclick = () => {
+    filterBar.style.display = 'none';
+
+}
+
+filterBar.onclick = () => {
+    filterBar.classList.remove('fa-times');
+
+}
 /* plugin */
 
 var swiper = new Swiper(".home-slider", {
